80d6dd044c502629f1903da4ccbffcb3b6fa5fcd
VIVI <vivienlenard00@gmail.com>
2023-02-22 17:18:16 +0100

    header

--- include/minishell.h
+++ include/minishell.h
@@ -26,12 +26,11 @@ typedef enum e_tokentype
 	is_pipe,
 }	t_tokentype;
 
-
 typedef struct s_token
 {
 	char			*content;
 	struct s_token	*next;
-	t_tokentype	type;
+	t_tokentype		type;
 }				t_token;
 
 typedef struct s_exec
@@ -49,29 +48,27 @@ typedef struct s_data
 	struct s_exec	**execs;
 }	t_data;
 
-int			main (void);
+int			main(void);
 char		*prompt(void);
 char		**split_token(char *str);
 int			load_tokens(char **split_tokens, t_data *data);
 t_tokentype	content_analyse(t_token *token);
 
-
 //Utils
-int		is_char(char place, char c);
-void	freestrings(char *s1, char *s2, char *s3, char **array);
-char	*username(void);
-void	print_tokens (t_token **token);
-void	free_data(t_data *data);
+int			is_char(char place, char c);
+void		freestrings(char *s1, char *s2, char *s3, char **array);
+char		*username(void);
+void		print_tokens(t_token **token);
+void		free_data(t_data *data);
 
-//Parser
-
-char	**allocate(char **split, int strnumber);
-char	**makestring(char **split, char *str, int start, int i);
-int		is_delimiter(char c);
-int		jump_delimiters(char *str, int *i);
-int		jump_redir(char *str, int *i);
-int		jump_delimiter_split(char **split, char *str, int *start, int *i);
-void	switch_flags(int *flag, int quote, int *keep_quote);
-int		handle_quote(char *str, int *i, int *flag, int *keep_quote);
+//Lexer
+char		**allocate(char **split, int strnumber);
+char		**makestring(char **split, char *str, int start, int i);
+int			is_delimiter(char c);
+int			jump_delimiters(char *str, int *i);
+int			jump_redir(char *str, int *i);
+int			jump_delimiter_split(char **split, char *str, int *start, int *i);
+void		switch_flags(int *flag, int quote, int *keep_quote);
+int			handle_quote(char *str, int *i, int *flag, int *keep_quote);
 
 #endif
