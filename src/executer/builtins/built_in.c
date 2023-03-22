c9661000289aa00f02b2227c3f2366275d7e688b
Michael Graefen <michaelgraefen@gmail.com>
2023-03-22 15:53:03 +0100

    it's working

--- src/executer/builtins/built_in.c
+++ src/executer/builtins/built_in.c
@@ -62,7 +62,7 @@ int is_num(char *str)
 	return(1);
 }
 
-void ft_exit(char **args)
+int ft_exit(char **args)
 {
 	long	exit_num;
 	
@@ -72,37 +72,56 @@ void ft_exit(char **args)
 	{
 		ft_putendl_fd("exit", 2);
 		ft_putendl_fd("minishell: exit: too many arguments", 2);
-		return ;
+		return (exit(g_errno), g_errno);
 	}
 	if(!is_num(args[1]))
 	{
 		ft_putstr_fd("exit: ", 2);
 		ft_putstr_fd(args[1], 2);
 		ft_putendl_fd(": numeric argument required", 2);
-		exit(255);
+		return (exit(255), 255);;
 	}
 	exit_num = ft_atoi(args[1]);
 	if(exit_num < 0 || exit_num > 255)
 		exit_num = exit_num % 256;
-	exit(exit_num);
+	return(exit(exit_num), exit_num);
 }
 
 int	built_in(t_exec *exec, char **env, t_data *data)
 {
 	if (ft_strncmp(exec->command, "echo", 5) == 0 || ft_strncmp(exec->command, "/bin/echo", 10) == 0)
-	 	return (ft_echo(exec, data), 1);
+	 	return (g_errno = ft_echo(exec, data), 1);
 	else if (ft_strncmp(exec->command, "cd", 3) == 0)
 		return(g_errno = ft_cd(exec, env), 1);
 	else if (ft_strncmp(exec->command, "pwd", 4) == 0)
-		return(ft_pwd(), exit(EXIT_SUCCESS), 1);
+		return(g_errno = ft_pwd(), 1);
 	else if (ft_strncmp(exec->command, "export", 7) == 0)
 		return (g_errno = ft_export(exec->args, &data->env), 1);
 	else if (ft_strncmp(exec->command, "unset", 6) == 0)
 		return (g_errno = ft_unset(exec->args, &data->env), 1);
 	else if (ft_strncmp(exec->command, "env", 4) == 0)
-		return(ft_env(data->env), exit(g_errno), 1);
+		return(g_errno = ft_env(data->env), exit(g_errno), 1);
 	else if (ft_strncmp(exec->command, "exit", 5) == 0)
-	 	return(ft_exit(exec->args), 1);
+	 	return(g_errno = ft_exit(exec->args), 1);
+	return (0);
+}
+
+int	built_in_child(t_exec *exec, char **env, t_data *data)
+{
+	if (ft_strncmp(exec->command, "echo", 5) == 0 || ft_strncmp(exec->command, "/bin/echo", 10) == 0)
+	 	return (g_errno = ft_echo(exec, data), exit(g_errno), 1);
+	else if (ft_strncmp(exec->command, "cd", 3) == 0)
+		return(g_errno = ft_cd(exec, env), exit(g_errno), 1);
+	else if (ft_strncmp(exec->command, "pwd", 4) == 0)
+		return(g_errno = ft_pwd(), exit(g_errno), 1);
+	else if (ft_strncmp(exec->command, "export", 7) == 0)
+		return (g_errno = ft_export(exec->args, &data->env), exit(g_errno), 1);
+	else if (ft_strncmp(exec->command, "unset", 6) == 0)
+		return (g_errno = ft_unset(exec->args, &data->env), exit(g_errno), 1);
+	else if (ft_strncmp(exec->command, "env", 4) == 0)
+		return(g_errno = ft_env(data->env), exit(g_errno), 1);
+	else if (ft_strncmp(exec->command, "exit", 5) == 0)
+	 	return(g_errno = ft_exit(exec->args), exit(g_errno), 1);
 	return (0);
 }
  
