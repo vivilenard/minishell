cbde26c0b688630a01afa0b3d5b38a5fca1341b5
Michael Graefen <michaelgraefen@gmail.com>
2023-03-30 11:26:45 +0200

    added libft

--- src/executer/builtins/built_in_utils.c
+++ src/executer/builtins/built_in_utils.c
@@ -5,47 +5,13 @@ int	char_is_in_str(char *str, char c)
 	int	i;
 
 	i = 0;
-	while(str[i])
+	while (str[i])
 	{
-		if(str[i] == c)
-			return(1);
+		if (str[i] == c)
+			return (1);
 		i++;
 	}
-	return(0);
-}
-
-char	*ft_strjoin_free_opt(char *s1, char *s2, int free_s1, int free_s2)
-{
-	char	*str;
-	size_t	i;
-	size_t	len;
-	size_t	n;
-
-	len = ft_strlen(s1) + ft_strlen(s2) + 1;
-	str = malloc(len * sizeof(char));
-	if (!str)
-		return (NULL);
-	i = 0;
-	while (s1 && s1[i] != '\0')
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	n = 0;
-	while (s2 && s2[n] != '\0')
-	{
-		str[i] = s2[n];
-		i++;
-		n++;
-	}
-	str[len - 1] = '\0';
-	if (free_s1 && s1 != NULL)
-		free (s1);
-	if (free_s2 && s2 != NULL)
-		free (s2);
-	if (ft_strlen(str) == 0)
-		return (free(str), NULL);
-	return (str);
+	return (0);
 }
 
 char	*ft_strjoin_s_e(char **args, int start, int end, char *c)
